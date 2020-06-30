7dd1485e1a1a5bdc409cf52adec95b593e2645d2
Kamil <tomala.kamil@gmail.com>
2020-06-30 11:44:26 +0200

    Create components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import "./App.css";
+import Tetris from "./components/Tetris";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Learn React</header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Tetris />
+  </div>
+);
 
 export default App;
