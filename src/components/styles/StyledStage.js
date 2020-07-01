1e0ddc0b3161226e2f6f83b8a8434d49a27ebbfa
Kamil <tomala.kamil@gmail.com>
2020-07-01 11:43:07 +0200

    Collision detection

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -4,13 +4,14 @@ export const StyledStage = styled.div`
   display: grid;
   grid-template-rows: repeat(
     ${(props) => props.height},
-    calc(25vh / ${(props) => props.width})
+    calc(25vw / ${(props) => props.width})
   );
   grid-template-columns: repeat(${(props) => props.width}, 1fr);
   grid-gap: 1px;
   border: 2px solid #333;
   width: 100%;
-  max-width: 60vh;
-  height: 300px;
+  max-width: 25vw;
   background: #111;
+
+  height: 400px;
 `;
--- src/tetrominos.js
+++ src/tetrominos.js
@@ -59,7 +59,7 @@ export const TETROMINOS = {
 };
 
 export const randomTetromino = () => {
-  const tetrominos = "JILOSTZ";
+  const tetrominos = "IJLOSTZ";
   const randTetromino =
     tetrominos[Math.floor(Math.random() * tetrominos.length)];
   return TETROMINOS[randTetromino];
